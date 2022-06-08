bd6bbf3e2b80181fd5a6d9799a5a81ae33bb5777
Matheus Kaczan <matheuskp29@gmail.com>
2022-06-08 01:20:49 -0300

    endpoint insert User criado.

--- src/main/java/com/matheuskaczan/workshopmongo/services/UserService.java
+++ src/main/java/com/matheuskaczan/workshopmongo/services/UserService.java
@@ -1,5 +1,6 @@
 package com.matheuskaczan.workshopmongo.services;
 
+import com.matheuskaczan.workshopmongo.dto.UserDTO;
 import com.matheuskaczan.workshopmongo.entities.User;
 import com.matheuskaczan.workshopmongo.repository.UserRepository;
 import com.matheuskaczan.workshopmongo.services.exception.ObjectNotFoundException;
@@ -22,5 +23,13 @@ public class UserService {
     public User findById(String id) {
         Optional<User> obj = userRepository.findById(id);
         return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+    }
+
+    public User insert(User obj) {
+        return userRepository.insert(obj);
+    }
+
+    public User fromDTO(UserDTO objDto) {
+        return new User(objDto.getId(), objDto.getNome(), objDto.getEmail());
     }
 }
